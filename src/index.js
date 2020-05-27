c5727c7f8936d4b97d44c40e7771d7efcca71c17
Yangxi-FAN <yxfan@thoughtworks.com>
2020-05-28 01:51:25 +0800

    Fix lint issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
+// eslint-disable-next-line import/no-unresolved
 import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line import/no-unresolved
 import { View } from 'react-native';
+// eslint-disable-next-line import/no-unresolved
 import Svg, { Path } from 'react-native-svg';
 import barcodes from 'jsbarcode/src/barcodes';
 
@@ -10,7 +13,7 @@ const encode = (text, Encoder, options) => {
   if (typeof text !== 'string' || text.length === 0) {
     throw new Error('Barcode value must be a non-empty string');
   }
-  
+
   let encoder;
   try {
     encoder = new Encoder(text, options);
@@ -18,12 +21,12 @@ const encode = (text, Encoder, options) => {
     // If the encoder could not be instantiated, throw error.
     throw new Error('Invalid barcode format.');
   }
-  
+
   // If the input is not valid for the encoder, throw error.
   if (!encoder.valid()) {
     throw new Error('Invalid barcode for selected format.');
   }
-  
+
   // Make a request for the binary data (and other information) that should be rendered
   // encoded structure is {
   //  text: 'xxxxx',
@@ -33,70 +36,76 @@ const encode = (text, Encoder, options) => {
   return encoded;
 };
 
-const drawRect = (x, y, rectWidth, height) => {
-  return `M${x},${y}h${rectWidth}v${height}h-${rectWidth}z`;
-};
+const drawRect = (x, y, rectWidth, height) => `M${x},${y}h${rectWidth}v${height}h-${rectWidth}z`;
 
 const drawSvgBarCode = (encoding, options = {}) => {
   const rects = [];
   // binary data of barcode
   const binary = encoding.data;
-  
+
   let barWidth = 0;
   let x = 0;
-  let yFrom = 0;
-  
+  const yFrom = 0;
+
+  // eslint-disable-next-line no-plusplus
   for (let b = 0; b < binary.length; b++) {
     x = b * options.singleBarWidth;
     if (binary[b] === '1') {
+      // eslint-disable-next-line no-plusplus
       barWidth++;
     } else if (barWidth > 0) {
       rects[rects.length] = drawRect(
         x - options.singleBarWidth * barWidth,
         yFrom,
         options.singleBarWidth * barWidth,
-        options.height
+        options.height,
       );
       barWidth = 0;
     }
   }
-  
+
   // Last draw is needed since the barcode ends with 1
   if (barWidth > 0) {
     rects[rects.length] = drawRect(
       x - options.singleBarWidth * (barWidth - 1),
       yFrom,
       options.singleBarWidth * barWidth,
-      options.height
+      options.height,
     );
   }
-  
+
   return rects;
 };
 
 export default function Barcode(props) {
-  const { value, format, singleBarWidth, maxWidth, height, lineColor, backgroundColor, onError } = props;
-  const [ bars, setBars ] = useState([]);
-  const [ barCodeWidth, setBarCodeWidth ] = useState(0);
-  const [ barCodeContainerWidth, setBarCodeContainerWidth ] = useState(0);
-  
+  const {
+    // eslint-disable-next-line no-unused-vars
+    value, format, singleBarWidth, maxWidth, height, lineColor, backgroundColor, onError,
+  } = props;
+  const [bars, setBars] = useState([]);
+  const [barCodeWidth, setBarCodeWidth] = useState(0);
+  const [barCodeContainerWidth, setBarCodeContainerWidth] = useState(0);
+
   useEffect(() => {
     const encoder = barcodes[format];
     const encoded = encode(value, encoder, props);
-    
+
     const resutBarcodeWidth = encoded.data.length * singleBarWidth;
     if (encoded) {
       setBars(drawSvgBarCode(encoded, props));
       setBarCodeWidth(resutBarcodeWidth);
       setBarCodeContainerWidth((maxWidth && resutBarcodeWidth > maxWidth) ? maxWidth : resutBarcodeWidth);
     }
-  }, [ value, format, singleBarWidth, maxWidth, height, lineColor ]);
-  
-  const containerStyle = { width: barCodeContainerWidth, height: height, backgroundColor: backgroundColor };
+  }, [value, format, singleBarWidth, maxWidth, height, lineColor]);
+
+  const containerStyle = { width: barCodeContainerWidth, height, backgroundColor };
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <View style={containerStyle}>
       <Svg
-        width={'100%'} height={'100%'} fill={lineColor}
+        width="100%"
+        height="100%"
+        fill={lineColor}
         viewBox={`0 0 ${barCodeWidth} ${height}`}
         preserveAspectRatio="none"
       >
